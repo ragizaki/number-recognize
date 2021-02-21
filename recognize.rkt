975489b3e87f5890b60d107b7537cda2318988f5
Zaki Machfj <zmachfj@uwaterloo.ca>
2021-02-21 00:53:09 -0500

    Update recognize.rkt

--- recognize.rkt
+++ recognize.rkt
@@ -1,14 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname recognize) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;;
-;; *************************************
-;;    Zaki Machfj
-;;    CS 135 Fall 2020
-;;    Assignment 4, Problem 3
-;; *************************************
-;;
-
 
 ;;
 ;; ***************************************************
